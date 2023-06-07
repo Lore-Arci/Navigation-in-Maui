b025c8c601b1e006f98626b1bd53b57d2a029240
Lore6 <arcidiacono.lorenzo2@gmail.com>
2023-06-07 21:41:42 +0200

    Start with navbar

--- Navigation/App.xaml.cs
+++ Navigation/App.xaml.cs
@@ -8,7 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		//MainPage = new NavigationPage(new NavPage1());
-		MainPage = new AppShell();
+		MainPage = new NavigationPage(new NavPage1());
+		//MainPage = new AppShell();
 	}
 }
